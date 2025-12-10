47d2ceb723cae2966b566c6e278b24abc4cc4f58
ericatici <erica.ticianelli@adejo.com.br>
2025-12-10 13:54:35 -0300

    test1

--- src/test/java/com/lanchonete/customer/bdd/CucumberSpringConfiguration.java
+++ src/test/java/com/lanchonete/customer/bdd/CucumberSpringConfiguration.java
@@ -1,13 +1,15 @@
 package com.lanchonete.customer.bdd;
 
+import com.lanchonete.customer.CustomerServiceApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@ContextConfiguration(classes = CustomerServiceApplication.class)
 public class CucumberSpringConfiguration {
 }
-
